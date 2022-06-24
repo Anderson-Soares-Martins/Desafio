0ce568d25968a542a8172e113f5afe5a85e0e8ef
Anderson Soares Martins <andersoaresmartins@gmail.com>
2022-06-24 12:43:57 -0300

    Add

--- app/src/main/java/com/example/heroes/data/adapter/viewHolder/CharacterAdapertViewHolder.kt
+++
@@ -1,7 +0,0 @@
-package com.example.heroes.data.adapter
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-import com.example.heroes.data.response.CharacterResponse
-
-class CharactersAdapterViewHolder
